2b385eae52be5d9e851b1d395c806c8f1a4d4722
25520814 <25520814@gm.uit.edu.vn>
2026-04-04 18:20:33 +0700

    them dungtuong
    Added wall collision detection and game over condition.

--- ConRan/ConRan/ConRan.cpp
+++ ConRan/ConRan/ConRan.cpp
@@ -8,20 +8,25 @@
 #define MAXX 35
 #define MAXY 20
 using namespace std;
+
 void gotoxy( int column, int line );
+
 struct Point{
     int x,y;
 };
+
 class CONRAN{
 public:
     struct Point A[100];
     int DoDai;
+
     CONRAN(){
         DoDai = 3;
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
+
     void Ve(Point Qua){
         for (int i = 0; i < DoDai; i++){
             gotoxy(A[i].x,A[i].y);
@@ -29,37 +34,50 @@ public:
         }
         gotoxy(Qua.x, Qua.y); cout<<"*";
     }
+
     void DiChuyen(int Huong, Point& Qua){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
+
         if (Huong==0) A[0].x = A[0].x + 1;
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
+
         if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
             DoDai++;
             Qua.x = rand()%(MAXX-MINX)+MINX;
             Qua.y = rand()%(MAXY-MINY)+MINY;
         }
     }
+
+    // THÊM: kiểm tra đụng tường
+    bool DungTuong(){
+        return (A[0].x <= MINX || A[0].x >= MAXX ||
+                A[0].y <= MINY || A[0].y >= MAXY);
+    }
 };
+
 void VeKhung(){
     for (int i = MINX ; i<=MAXX ; i++)
-        for (int j = MINX ; j<=MAXY ; j++)
+        for (int j = MINY ; j<=MAXY ; j++)
             if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
-            gotoxy(i,j);
-            printf("+");
-        }
+                gotoxy(i,j);
+                printf("+");
+            }
 }
+
 int main()
 {
     CONRAN r;
     int Huong = 0;
     char t;
     Point Qua;
+
     srand((int)time(0));
     Qua.x = rand()%(MAXX-MINX)+MINX;
     Qua.y = rand()%(MAXY-MINY)+MINY;
+
     while (1){
         if (kbhit()){
             t = getch();
@@ -68,24 +86,32 @@ int main()
             if (t=='d') Huong = 0;
             if (t=='x') Huong = 1;
         }
+
         system("cls");
         VeKhung();
         r.Ve(Qua);
         r.DiChuyen(Huong, Qua);
+
+        // THÊM: Game Over khi đụng tường
+        if (r.DungTuong()){
+            system("cls");
+            cout<<"GAME OVER!";
+            break;
+        }
+
         Sleep(300);
     }
 
     return 0;
 }
 
-
 void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(
+        GetStdHandle( STD_OUTPUT_HANDLE ),
+        coord
     );
-  }
+}
